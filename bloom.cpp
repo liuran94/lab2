286fcd52a087c019d059223b1a2e1ca411261cb4
MQZ <821404356@qq.com>
2017-12-04 13:36:29 +0800

    Fix bugs of matrix count and remove test code in main (#3)

--- bloom.cpp
+++ bloom.cpp
@@ -14,9 +14,6 @@ int p_table[MAX_URLPATH_NUM] = { 0 };
 int myEncrypt(char *str, char *key){
     assert(str != NULL);
     char *cipher = strdup(str);
-    /*  int ecb_crypt(char *key, char *data, \
-                   unsigned datalen,unsigned mode);
-    */
     ecb_crypt(key, cipher, strlen(str), DES_ENCRYPT);	/*第一次映射函数采用ecb_crypt */
     int i;
     int var = 0;
@@ -28,10 +25,11 @@ int myEncrypt(char *str, char *key){
     return var;
 }
 
+
 /*判断path是否存在过，如果不存在就把它记入PathBloomTable中*/
 int bloomFilter(char *path){
 
-    int mod = 33 * MAX_URLPATH_NUM;
+    int mod = 32 * MAX_URLPATH_NUM;
     int flag = 0;
     //printf("before\n");
 
@@ -48,8 +46,8 @@ int bloomFilter(char *path){
         free(key);
         srand(f[i]);
         g[i] = rand() % mod;	/* rand() * 33 * MAX_URLPATH_NUM */
-        int index = g[i] / 33;
-        int pos = g[i] % 33;
+        int index = g[i] / 32;
+        int pos = g[i] % 32;
         if (p_table[index] & (0x80000000 >> pos))//如果与p_table[index]相同
             flag++;
         else
--- matrix.h
+++ matrix.h
@@ -29,5 +29,6 @@ void generateA();
 void initPageRank();
 void generatePageRank();
 void printPageRank();
+void quickSortForDouble(double* arr,int startPos, int endPos);
 
 #endif //LAB2_MATRIX_H
